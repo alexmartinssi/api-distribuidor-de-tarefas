14bf6c03741410ec2b5d9d53e8c472fb87f207ef
alexmartinssi <alexmbezerrajp@gmail.com>
2018-11-15 21:15:12 -0300

    Implementando autorização.

--- src/main/java/com/br/apipadrao/security/WebSecurityConfig.java
+++ src/main/java/com/br/apipadrao/security/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll() //
 		.anyRequest().authenticated(); //
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil)); //
-      //  http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService)); //
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService)); //
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); //
     }
     
