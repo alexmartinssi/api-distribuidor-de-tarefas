cedee2ee4207940b5978b85b26cdbbc90065c9b0
alexmartinssi <alexmbezerrajp@gmail.com>
2018-11-17 21:24:40 -0300

    Refresh Token.

---
+++ src/main/java/com/br/apipadrao/resources/AuthResources.java
@@ -0,0 +1,47 @@
+package com.br.apipadrao.resources;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andersonmarques.cursomc.dto.EmailDTO;
+import com.andersonmarques.cursomc.services.AuthService;
+import com.br.apipadrao.security.JWTUtil;
+import com.br.apipadrao.security.UserSS;
+import com.br.apipadrao.services.UserService;
+
+@RestController
+@RequestMapping(value="/auth")
+public class AuthResources {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private AuthService authService;
+	
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = userService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		
+		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
+
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+	public ResponseEntity<Void> forgot (@Valid @RequestBody EmailDTO objDTO) {
+		authService.sandNewPassword(objDTO.getEmail());
+		return ResponseEntity.noContent().build();
+	}
+}
